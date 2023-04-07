066bb9efe938a9ccfc295e78d58dfc112ad8f2f5
hung <ff789456741@gmail.com>
2023-04-07 14:20:14 +0800

    add: member & member model

--- src/App.vue
+++ src/App.vue
@@ -1,107 +1,134 @@
 <template>
-  <div class="container">    
+  <member-modal :show="memberShow" @close="closeMember" @add-member="addMember"></member-modal>
+  <div class="container">
     <main>
+      <Members :members="members" @show-member="showMember"> </Members>
     </main>
     <footer>
       <button class="share-payment">分帳款項</button>
       <button class="add-payment">&#x2b;</button>
       <button class="end-share">結束分帳</button>
-    </footer>    
-  </div>  
+    </footer>
+  </div>
 </template>
 
-<script setup>  
+<script setup>
+import { v4 as uuidv4 } from 'uuid';
+
+import Members from './components/Members.vue'
+import memberModal from './components/AddMembers.vue'
+
+import { ref, reactive } from 'vue'
+
+const members = reactive([])
+let memberShow = ref(false)
+
+function showMember() {
+  memberShow.value = true
+}
+
+function closeMember() {
+  memberShow.value = false
+}
+
+function addMember(name) {
+  const member = {
+    id: uuidv4(),
+    name,
+    price: 0
+  }
+  members.push(member);
+}
 </script>
 
-<style>  
-  .container {
-    position: relative;
-    display: block;
-    width: 300px;
-    background-color: var(--main-background);
-    height: 600px;
-    border-radius: 10px;
-    margin: auto;
-    overflow: hidden;
-  }
+<style>
+.container {
+  position: relative;
+  display: block;
+  width: 300px;
+  background-color: var(--main-background);
+  height: 600px;
+  border-radius: 10px;
+  margin: auto;
+  overflow: hidden;
+}
 
-  main {
-    margin-top: 20px;
-    height: 90%;
-    overflow: auto;
-  }
+main {
+  margin-top: 20px;
+  height: 90%;
+  overflow: auto;
+}
 
-  main::-webkit-scrollbar {
-    width: 7px;
-  }
+main::-webkit-scrollbar {
+  width: 7px;
+}
 
-  main::-webkit-scrollbar-button {
-    background: transparent;
-    border-radius: 4px;
-  }
+main::-webkit-scrollbar-button {
+  background: transparent;
+  border-radius: 4px;
+}
 
-  main::-webkit-scrollbar-track-piece {
-    background: transparent;
-  }
+main::-webkit-scrollbar-track-piece {
+  background: transparent;
+}
 
-  main::-webkit-scrollbar-thumb {
-    border-radius: 4px;
-    background-color: rgba(0, 0, 0, 0.4);
-    border: 1px solid slategrey;
-  }
+main::-webkit-scrollbar-thumb {
+  border-radius: 4px;
+  background-color: rgba(0, 0, 0, 0.4);
+  border: 1px solid slategrey;
+}
 
-  main::-webkit-scrollbar-track {
-    box-shadow: transparent;
-  }
-  
-  footer {
-    position: absolute;
-    display: flex;
-    justify-content: space-between;
-    background-color: var(--main-background);
-    left: 0;
-    bottom: 0;
-    width: 100%;
-    padding: 0 20px;
-    height: 50px;
-    border-top: 2px solid #000;
-    z-index: 2;
-  }
+main::-webkit-scrollbar-track {
+  box-shadow: transparent;
+}
 
-  footer button{
-    cursor: pointer;
-    border: none;
-    background-color: var(--main-background);
-    font-size: 1.2rem;
-    font-weight: bold;
-  }
+footer {
+  position: absolute;
+  display: flex;
+  justify-content: space-between;
+  background-color: var(--main-background);
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  height: 50px;
+  border-top: 2px solid #000;
+  z-index: 2;
+}
 
-  .share-payment,
-  .end-share {
-    transform: scale(.9);
-  }
+footer button {
+  cursor: pointer;
+  border: none;
+  background-color: var(--main-background);
+  font-size: 1.2rem;
+  font-weight: bold;
+}
 
-  .share-payment:hover,
-  .end-share:hover{
-    transform: scale(1);
-  }
+.share-payment,
+.end-share {
+  transform: scale(0.9);
+}
 
-  .add-payment {
-    position: absolute;
-    left: 50%;
-    top: 0;
-    transform: translate(-50%, -50%) scale(.9);
-    background-color: var(--main-background);
-    border: 2px solid #000;
-    width: 70px;
-    height: 70px;
-    border-radius: 50%;
-    font-size: 3.5rem;
-    font-weight: normal;    
-  }
+.share-payment:hover,
+.end-share:hover {
+  transform: scale(1);
+}
 
-  .add-payment:hover {
-    transform: translate(-50%, -50%) scale(1);
-  }
-  
+.add-payment {
+  position: absolute;
+  left: 50%;
+  top: 0;
+  transform: translate(-50%, -50%) scale(0.9);
+  background-color: var(--main-background);
+  border: 2px solid #000;
+  width: 70px;
+  height: 70px;
+  border-radius: 50%;
+  font-size: 3.5rem;
+  font-weight: normal;
+}
+
+.add-payment:hover {
+  transform: translate(-50%, -50%) scale(1);
+}
 </style>
---
+++ src/components/Members.vue
@@ -0,0 +1,88 @@
+<template>
+  <ul v-if="members.length !== 0">
+    <li v-for="member of members" :key="member.id" v-cloak>
+      <p>{{ member.name }}</p>
+      <div class="price"><span>NT$ </span>{{ member.price }}</div>
+    </li>
+  </ul>
+  <div class="no-member" v-else>目前無成員</div>
+  <button class="add-member" @click="showMember">&#x2b; 新增成員</button>
+</template>
+
+<script setup>
+const props = defineProps(['members'])
+const emit = defineEmits(['show-member'])
+
+function showMember() {
+  emit('show-member')
+}
+</script>
+
+<style scoped>
+ul {
+  padding: 0 20px;
+}
+
+li {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid #000;
+}
+
+li + li {
+  margin-top: 20px;
+}
+
+.price,
+p {
+  font-size: 1.2rem;
+}
+
+.price {
+  flex-shrink: 0;
+  font-weight: bold;
+}
+
+.price span {
+  font-size: 0.8rem;
+  font-weight: normal;
+}
+
+.receivables {
+  color: var(--receivables);
+}
+
+.payable {
+  color: var(--payable);
+}
+
+.no-member {
+  font-size: 24px;
+  color: var(--no-one);
+  white-space: nowrap;
+  margin: 0 auto;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.add-member {
+  display: block;
+  width: 80%;
+  border: 2px solid #000;
+  border-radius: 50px;
+  padding: 5px 0;
+  font-size: 24px;
+  font-weight: bold;
+  cursor: pointer;
+  background-color: transparent;
+  margin: 20px auto 50px;
+  transform: scale(0.9);
+}
+
+.add-member:hover {
+  transform: scale(1);
+}
+</style>
